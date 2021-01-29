10506f9c0d1305a33547813a3f0fa589c2e02f9e
samurotko <samu.rotko@aalto.fi>
2021-01-29 15:59:09 +0200

    rigging again

--- full-stack-open-pokedex/app.js
+++ full-stack-open-pokedex/app.js
@@ -1,23 +1,23 @@
-// require('dotenv').config()
-// const express = require('express')
-// const app = express()
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
-// // Heroku dynamically sets a port
-// var server_port = process.env.PORT || 5000
-// var server_host = process.env.MY_HOST || '0.0.0.0'
+// Heroku dynamically sets a port
+var server_port =  5000
+var server_host = process.env.MY_HOST || '0.0.0.0'
 
-// app.use(express.static('dist'))
+app.use(express.static('dist'))
 
-// app.get('/health', (req, res) => {
-//   res.send('ok')
-// })
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
-// app.get('/version', (req, res) => {
-//   res.send('1') // change this string to ensure a new version deployed
-// })
-// //server_host, proc worker: node app.js -p $PORT
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+//server_host, proc worker: node app.js -p $PORT
 
-// app.listen(server_port, server_host,() => {
-//   console.log('server started on port:', process.env.PORT)
-//   console.log('process',process.env)
-// })
+app.listen(server_port, server_host,() => {
+  console.log('server started on port:', server_port)
+  console.log('process',process.env)
+})
