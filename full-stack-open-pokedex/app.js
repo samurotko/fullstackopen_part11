4ab2ff0d985740b8886fef887e0339e749e13f88
samurotko <samu.rotko@aalto.fi>
2021-01-29 14:32:48 +0200

    health cheeekc

--- full-stack-open-pokedex/app.js
+++ full-stack-open-pokedex/app.js
@@ -8,13 +8,13 @@ var server_port = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-// app.get('/health', (req, res) => {
-//   res.send('ok')
-// })
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
-// app.get('/version', (req, res) => {
-//   res.send('1') // change this string to ensure a new version deployed
-// })
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
 app.listen(server_port, '0.0.0.0', () => {
   console.log('server started on port:', process.env.PORT)
