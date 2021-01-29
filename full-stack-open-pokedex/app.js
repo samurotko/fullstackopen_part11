7e23664a8ee3108281ac00f53be9b425df3dfda8
samurotko <samu.rotko@aalto.fi>
2021-01-29 14:35:27 +0200

    hosting

--- full-stack-open-pokedex/app.js
+++ full-stack-open-pokedex/app.js
@@ -4,7 +4,7 @@ const app = express()
 
 // Heroku dynamically sets a port
 var server_port = process.env.PORT || 5000
-//var server_host = process.env.MY_HOST || '0.0.0.0'
+var server_host = process.env.MY_HOST || '0.0.0.0'
 
 app.use(express.static('dist'))
 
@@ -16,7 +16,7 @@ app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
 
-app.listen(server_port, '0.0.0.0', () => {
+app.listen(server_port, server_host, () => {
   console.log('server started on port:', process.env.PORT)
   console.log('process',process.env)
 })
